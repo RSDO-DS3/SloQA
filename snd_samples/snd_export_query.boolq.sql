c76130600fe8c3bb41eb1a777c7ec7e5495259a0
Mladen Borovič <mladen.borovic@gmail.com>
2022-06-07 09:46:47 +0200

    Codebase update

--- snd_samples/snd_export_query.boolq.sql
+++
@@ -1,7 +0,0 @@
-SELECT berta_document_exercises.id, berta_documents.id, berta_document_exercises.type_id, berta_document_exercises.text, berta_document_exercises.alternatives, berta_documents.plain_text
-FROM berta_document_exercises
-LEFT JOIN berta_documents ON berta_documents.id = berta_document_exercises.document_id
-WHERE (
-	(berta_document_exercises.type_id = 1 AND berta_document_exercises.subtype_id IS NULL) 
-	OR berta_document_exercises.type_id = 2
-) AND berta_documents.status_id <> 4 ORDER BY berta_documents.id ASC
